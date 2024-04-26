913dcee8715bf38951bef73c7336ae2f51506620
Theron <theronjpb@al.insper.edu.br>
2024-04-26 15:30:39 -0300

    titulo

---
+++ codigo_principal.py
@@ -0,0 +1,5 @@
+print(' ===================================== ')
+print('|                                     |')
+print('| Bem-vindo ao INSPER - Batalha Naval |')
+print('|                                     |')
+print(' =======   xxxxxxxxxxxxxxxxx   ======= ')
