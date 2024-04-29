02cd4ee20ff27134d52e2f4dc54448fa5592ac35
Theron <theronjpb@al.insper.edu.br>
2024-04-29 15:17:14 -0300

    contador inicial

--- codigo_principal.py
+++ codigo_principal.py
@@ -1,9 +1,26 @@
+import time
+from funcoes_iniciais import cria_mapa
+from funcoes_iniciais import aloca_navios
+from funcoes_iniciais import foi_derrotado
+from funcoes_iniciais import posicao_suporta
+
 print(' ===================================== ')
 print('|                                     |')
 print('| Bem-vindo ao INSPER - Batalha Naval |')
 print('|                                     |')
 print(' =======   xxxxxxxxxxxxxxxxx   ======= ')
-from funcoes_iniciais import cria_mapa
-from funcoes_iniciais import aloca_navios
-from funcoes_iniciais import foi_derrotado
-from funcoes_iniciais import posicao_suporta
+print('digite qualquer coisa para continuar')
+u=input('')
+print('Jogo iniciando...')
+time.sleep(1)
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(1)
+
+mapa=cria_mapa(10)
+for i in mapa:
+    print(i)
+
